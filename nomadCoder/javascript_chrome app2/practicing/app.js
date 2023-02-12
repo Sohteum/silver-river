f9522b606f44d20a65709009761941fdca2a6e5c
Sohteum <thtwk1123@gmail.com>
2023-02-12 22:20:46 +0900

    add calculator

--- nomadCoder/javascript_chrome app2/practicing/app.js
+++ nomadCoder/javascript_chrome app2/practicing/app.js
@@ -92,3 +92,27 @@ const player2 = {
   },
 };
 player2.sayHello("sohteum");
+
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  multifly: function (a, b) {
+    console.log(a * b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(5, 1);
+calculator.minus(5, 1);
+calculator.divide(5, 1);
+calculator.multifly(5, 5);
+calculator.powerOf(5, 3);
