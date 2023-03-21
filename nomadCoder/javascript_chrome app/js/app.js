e8642da4b72804d979336759fde2bfe588bc161e
Sohteum <thtwk1123@gmail.com>
2023-03-21 21:42:33 +0900

    app  수정

--- nomadCoder/javascript_chrome app/js/app.js
+++ nomadCoder/javascript_chrome app/js/app.js
@@ -1,4 +1,4 @@
-const age = parseInt(prompt("How old are you?"));
+/* const age = parseInt(prompt("How old are you?"));
 
 if (isNaN(age) ||age <0) {
   console.log("Please write a positive number");
@@ -15,5 +15,5 @@ console.log("wow you are wise")
 }
 /* 결국 ||연산자에서는
  폴스가 되기 위해서는 모두 폴스여야하는거고 둘중 하나라도 트루면 트루 */
- /* 하지만 &&연산자에서는 
- 트루가 되기 위해서 모두 트루여야하는거고 둘중 하나라도 폴스면 폴스 */
+/* 하지만 &&연산자에서는
+트루가 되기 위해서 모두 트루여야하는거고 둘중 하나라도 폴스면 폴스 */
