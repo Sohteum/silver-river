a1f7b6d749c8dd4f9c7c746f4decc975872d0a01
Sohteum <thtwk1123@gmail.com>
2023-04-16 13:22:00 +0900

    .

--- nomadCoder/reactJS/react-for-beginners/src/App.js
+++ nomadCoder/reactJS/react-for-beginners/src/App.js
@@ -3,26 +3,40 @@ import ReactDom from "react-dom";
 
 import "./style.css";
 
-function App() {
+const [] = useState
 
-  const [] = useState
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  },
+]
 
-  const content = [
-    {
-      tab: "Section 1",
-      content: "I'm the content of the Section 1"
-    },
-    {
-      tab: "Section 2",
-      content: "I'm the content of the Section 2"
-    },
-  ]
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex]
+  }
+}
+
+const App = () => {
+
+  const tabs = useTabs(0, content);
 
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      {content.map(section => (
+        <button>{section.tab}</button>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
