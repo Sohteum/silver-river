d48230c63947a0f690c286af850f21793e426c45
Sohteum <thtwk1123@gmail.com>
2023-04-26 23:14:35 +0900

    .

--- nomadCoder/reactJS/react-masterclass/src/App.js
+++ nomadCoder/reactJS/react-masterclass/src/App.js
@@ -1,11 +1,29 @@
 import styled from "styled-components"
 
+const Father = styled.div`
+ display: flex;
+`;
+
+const Box = styled.div`
+background-color:${(props) => props.bgColor};
+width: 100px;
+height:100px;
+`;
+const Circle = styled(Box)`
+border-radius:50%;
+`;
+
+const Text = styled.h1`
+color:white;
+`;
+
+
 function App() {
   return (
-    <div>
-      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
-      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
-    </div>
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </Father>
   );
 }
 
