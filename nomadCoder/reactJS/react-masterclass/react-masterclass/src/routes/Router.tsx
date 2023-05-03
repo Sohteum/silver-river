8a319558bbb3e7ec796dc3ef24e156cc0c046271
Sohteum <thtwk1123@gmail.com>
2023-05-04 06:34:14 +0900

    .

---
+++ nomadCoder/reactJS/react-masterclass/react-masterclass/src/routes/Router.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+export default Router;
